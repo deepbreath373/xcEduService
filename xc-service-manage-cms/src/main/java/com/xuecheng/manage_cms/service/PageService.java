bd7be0f9f0dadd934bd5cffe38c7154c25d6e664
deepbreath373 <1149735109@qq.com>
2021-03-13 18:16:48 +0800

    扩展findAll方法，增加通过条件对象对数据库进行查询

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
@@ -6,10 +6,9 @@ import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.QueryResponseResult;
 import com.xuecheng.framework.model.response.QueryResult;
 import com.xuecheng.manage_cms.dao.CmsPageRepository;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 /**
@@ -35,6 +34,26 @@ public class PageService {
         if(queryPageRequest == null){
             queryPageRequest = new QueryPageRequest();
         }
+
+        //自定义条件查询
+        //定义条件匹配器
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching()
+                                            .withMatcher("pageAliase", ExampleMatcher.GenericPropertyMatchers.contains());
+        //条件值对象
+        CmsPage cmsPage = new CmsPage();
+        if(StringUtils.isNotEmpty(queryPageRequest.getSiteId())){
+            cmsPage.setSiteId(queryPageRequest.getSiteId());
+        }
+        if(StringUtils.isNotEmpty(queryPageRequest.getTemplateId())){
+            cmsPage.setTemplateId(queryPageRequest.getTemplateId());
+        }
+        if(StringUtils.isNotEmpty(queryPageRequest.getPageAliase())){
+            cmsPage.setPageAliase(queryPageRequest.getPageAliase());
+        }
+
+        //定义条件对象
+        Example<CmsPage> example = Example.of(cmsPage, exampleMatcher);
+
         //分页参数
         if(page <=0){
             page = 1;
@@ -44,7 +63,8 @@ public class PageService {
             size = 10;
         }
         Pageable pageable = PageRequest.of(page,size);
-        Page<CmsPage> all = cmsPageRepository.findAll(pageable);
+        //实现自定义条件查询并分页查询
+        Page<CmsPage> all = cmsPageRepository.findAll(example,pageable);
         QueryResult queryResult = new QueryResult();
         queryResult.setList(all.getContent());//数据列表
         queryResult.setTotal(all.getTotalElements());//数据总记录数
