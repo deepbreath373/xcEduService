0151cc44fb0dc17fe251afbe75fd38c800e9ef44
deepbreath373 <1149735109@qq.com>
2021-03-14 11:20:47 +0800

    实现了对cms_template表的查询

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsTemplateController.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsTemplateController.java
@@ -1,10 +1,7 @@
 package com.xuecheng.manage_cms.controller;
 
-import com.xuecheng.api.cms.CmsSiteControllerApi;
 import com.xuecheng.api.cms.CmsTemplateControllerApi;
-import com.xuecheng.framework.domain.cms.request.QuerySiteRequest;
 import com.xuecheng.framework.model.response.QueryResponseResult;
-import com.xuecheng.manage_cms.service.SiteService;
 import com.xuecheng.manage_cms.service.TemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +23,6 @@ public class CmsTemplateController implements CmsTemplateControllerApi {
     @Override
     @GetMapping("/list")
     public QueryResponseResult findList() {
-
         //调用service
         return templateService.findList();
     }
