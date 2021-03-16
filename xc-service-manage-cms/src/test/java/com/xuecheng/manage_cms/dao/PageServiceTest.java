a350baef9ce04678100898e4e8998e19c08d6748
deepbreath373 <1149735109@qq.com>
2021-03-16 22:40:27 +0800

    测试GridFS的存储功能

--- xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/dao/PageServiceTest.java
+++ xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/dao/PageServiceTest.java
@@ -1,11 +1,17 @@
 package com.xuecheng.manage_cms.dao;
 
 import com.xuecheng.manage_cms.service.PageService;
+import org.bson.types.ObjectId;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -13,9 +19,25 @@ public class PageServiceTest {
     @Autowired
     PageService pageService;
 
+    @Autowired
+    GridFsTemplate gridFsTemplate;
+
     @Test
     public void testGetPageHtml(){
         String pageHtml = pageService.getPageHtml("604f687007434a44d0313bef");
         System.out.println(pageHtml);
+    }
+
+    @Test
+    public void testGridFs() throws FileNotFoundException {
+        //要存储的文件
+        File file = new File("d:/index_banner.html");
+        //定义输入流
+        FileInputStream inputStram = new FileInputStream(file);
+        //向GridFS存储文件
+        ObjectId objectId = gridFsTemplate.store(inputStram, "轮播图测试文件01", "");
+        //得到文件ID
+        String fileId = objectId.toString();
+        System.out.println(fileId);
     }
 }
