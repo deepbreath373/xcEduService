3c91369a88592860080f8c5ed1a0092225d0b9de
deepbreath373 <1149735109@qq.com>
2021-03-20 19:02:55 +0800

    实现学成在线对课程计划的查询

---
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/controller/CourseController.java
@@ -0,0 +1,21 @@
+package com.xuecheng.manage_course.controller;
+
+import com.xuecheng.api.course.CourseControllerApi;
+import com.xuecheng.framework.domain.course.ext.TeachplanNode;
+import com.xuecheng.manage_course.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/course")
+public class CourseController implements CourseControllerApi {
+
+    @Autowired
+    CourseService courseService;
+
+    @Override
+    @GetMapping("/teachplan/list/{courseId}")
+    public TeachplanNode findTeachplanList(@PathVariable("courseId") String courseId) {
+        return courseService.findTeachplanList(courseId);
+    }
+}
