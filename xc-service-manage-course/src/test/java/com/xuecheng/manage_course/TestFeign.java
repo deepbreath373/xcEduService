aec205db7bbc0d920a9e07933288c754d45e4c69
deepbreath373 <1149735109@qq.com>
2021-03-23 21:09:47 +0800

    配置并测试feign的负载均衡

---
+++ xc-service-manage-course/src/test/java/com/xuecheng/manage_course/TestFeign.java
@@ -0,0 +1,29 @@
+package com.xuecheng.manage_course;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.manage_course.client.CmsPageClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ **/
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestFeign {
+    @Autowired
+    CmsPageClient cmsPageClient;
+    @Test
+    public void testFeign(){
+        CmsPage cmsPage = cmsPageClient.findCmsPageById("5a754adf6abb500ad05688d9");
+        System.out.println(cmsPage);
+    }
+}
