5f53a69a92022ab4a5518c6428090bb21c2cbbff
deepbreath373 <1149735109@qq.com>
2021-04-08 22:05:41 +0800

    实现用户认证的功能(密码授权模式)

--- xc-service-learning/src/main/java/com/xuecheng/learning/client/CourseSearchClient.java
+++ xc-service-learning/src/main/java/com/xuecheng/learning/client/CourseSearchClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "XC-SERVICE-SEARCH")
+@FeignClient(XcServiceList.XC_SERVICE_SEARCH)
 public interface CourseSearchClient {
     //根据课程计划id查询课程媒资
     @GetMapping("/search/course/getmedia/{teachplanId}")
