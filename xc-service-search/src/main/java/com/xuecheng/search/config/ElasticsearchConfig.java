4c425f0fd473e3f2edc503647502b48f5c6b429c
deepbreath373 <1149735109@qq.com>
2021-06-17 11:01:53 +0800

    增加获取 httpHost[] 方法，减少冗余代码

--- xc-service-search/src/main/java/com/xuecheng/search/config/ElasticsearchConfig.java
+++ xc-service-search/src/main/java/com/xuecheng/search/config/ElasticsearchConfig.java
@@ -17,9 +17,8 @@ public class ElasticsearchConfig {
     @Value("${xuecheng.elasticsearch.hostlist}")
     private String hostlist;
 
-    @Bean
-    public RestHighLevelClient restHighLevelClient(){
-        //解析hostlist配置信息
+    private HttpHost[] getHttpHost(){
+        //解析hostList配置信息
         String[] split = hostlist.split(",");
         //创建HttpHost数组，其中存放es主机和端口的配置信息
         HttpHost[] httpHostArray = new HttpHost[split.length];
@@ -27,6 +26,12 @@ public class ElasticsearchConfig {
             String item = split[i];
             httpHostArray[i] = new HttpHost(item.split(":")[0], Integer.parseInt(item.split(":")[1]), "http");
         }
+        return httpHostArray;
+    }
+
+    @Bean
+    public RestHighLevelClient restHighLevelClient(){
+        HttpHost[] httpHostArray = getHttpHost();
         //创建RestHighLevelClient客户端
         return new RestHighLevelClient(RestClient.builder(httpHostArray));
     }
@@ -34,14 +39,7 @@ public class ElasticsearchConfig {
     //项目主要使用RestHighLevelClient，对于低级的客户端暂时不用
     @Bean
     public RestClient restClient(){
-        //解析hostlist配置信息
-        String[] split = hostlist.split(",");
-        //创建HttpHost数组，其中存放es主机和端口的配置信息
-        HttpHost[] httpHostArray = new HttpHost[split.length];
-        for(int i=0;i<split.length;i++){
-            String item = split[i];
-            httpHostArray[i] = new HttpHost(item.split(":")[0], Integer.parseInt(item.split(":")[1]), "http");
-        }
+        HttpHost[] httpHostArray = getHttpHost();
         return RestClient.builder(httpHostArray).build();
     }
 
